7102befc19f2f5a44d2cc5a9da2696896b0f2e6e
Kyle Kloepper <kkloepper@cloudflare.com>
2018-07-19 08:58:35 -0500

    Initial commit showing error.

---
+++ sign_test.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+var crypto = require("@trust/webcrypto");
+async function signTest() {
+    let newKey = await crypto.subtle.generateKey(
+        {
+            name: 'RSASSA-PKCS1-v1_5',
+            modulusLength: 1024,
+            publicExponent: new Uint8Array([1, 0, 1]),
+            hash: { name: 'SHA-256' },
+        }
+        , true, ['sign']);
+    let signature = await crypto.subtle.sign("RSASSA-PKCS1-v1_5", newKey.privateKey, new Uint8Array(16));
+}
+
+signTest();
